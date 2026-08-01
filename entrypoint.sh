1663605a8636debd37e6ecd05493cca72edb579b
Ali Turan <alituran44@users.noreply.github.com>
2026-08-02 02:47:15 +0300

    Auto install all main applications and custom agency modules on Render database

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,13 +23,16 @@ check_config "db_user" "$USER"
 check_config "db_password" "$PASSWORD"
 check_config "http_port" "$HTTP_PORT"
 
+# Auto install all main apps and custom agency modules on Render cloud database
+INIT_MODULES="base,crm,sale_management,account,website,website_sale,mass_mailing,project,hr,hr_expense,hr_recruitment,hr_holidays,im_livechat,survey,fleet,repair,pos_restaurant,point_of_sale,mrp,social_media_community,marketing_ads_dashboard"
+
 case "$1" in
     -- | odoo)
         shift
-        exec odoo "$@" "${DB_ARGS[@]}"
+        exec odoo "$@" "${DB_ARGS[@]}" "-d" "odoo_db" "-i" "$INIT_MODULES"
         ;;
     -*)
-        exec odoo "$@" "${DB_ARGS[@]}"
+        exec odoo "$@" "${DB_ARGS[@]}" "-d" "odoo_db" "-i" "$INIT_MODULES"
         ;;
     *)
         exec "$@"
