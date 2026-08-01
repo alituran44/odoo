4a285833aea318fe71de9b1f00cb9585456dc293
Ali Turan <alituran44@users.noreply.github.com>
2026-08-02 02:44:10 +0300

    Bind Odoo HTTP port dynamically to Render PORT to resolve 502 Bad Gateway

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -e
 
-# Force database port to 5432, ignoring Render HTTP PORT environment variable
-export DB_PORT=5432
-export DB_PORT_5432_TCP_PORT=5432
+# Extract HTTP PORT assigned by Render (defaults to 8069)
+HTTP_PORT="${PORT:-8069}"
 
 : ${HOST:=${DB_HOST:-"127.0.0.1"}}
 : ${USER:=${DB_USER:-"odoo"}}
@@ -22,6 +21,7 @@ check_config "db_host" "$HOST"
 check_config "db_port" "5432"
 check_config "db_user" "$USER"
 check_config "db_password" "$PASSWORD"
+check_config "http_port" "$HTTP_PORT"
 
 case "$1" in
     -- | odoo)
